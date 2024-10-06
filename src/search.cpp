398fb5d916d8f316a4aac49da72e2d52b7de8d45
Ömer Faruk Tutkun <lrctutfaruk@gmail.com>
2024-10-06 22:02:32 +0300

    bench 1239251

--- src/search.cpp
+++ src/search.cpp
@@ -1034,6 +1034,9 @@ moves_loop:  // When in check, search starts here
 
                 lmrDepth += history / 3653;
 
+                //reduce less if corrhist score is high
+                lmrDepth += std::abs(ss->staticEval - unadjustedStaticEval) > 111;
+
                 Value futilityValue =
                   ss->staticEval + (bestValue < ss->staticEval - 51 ? 145 : 49) + 144 * lmrDepth;
 
