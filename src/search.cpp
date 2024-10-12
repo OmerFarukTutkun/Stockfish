8bcdb1e101b6bef09e27edcd4111b39796713845
Ömer Faruk Tutkun <lrctutfaruk@gmail.com>
2024-10-12 23:08:39 +0300

    bench 1600067

--- src/search.cpp
+++ src/search.cpp
@@ -91,8 +91,10 @@ Value to_corrected_static_eval(Value v, const Worker& w, const Position& pos, St
     int         cntcv = 1;
 
     if (m.is_ok())
+    {
         cntcv = int((*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]);
-
+        cntcv += int((*(ss - 3)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]);
+    }
     const auto cv =
       (5932 * pcv + 2994 * mcv + 3269 * macv + 5660 * micv + 6237 * (wnpcv + bnpcv) + cntcv * 5555)
       / 131072;
@@ -1437,7 +1439,10 @@ moves_loop:  // When in check, search starts here
           << bonus * 165 / 128;
 
         if (m.is_ok())
+        {
             (*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()] << bonus;
+            (*(ss - 3)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()] << bonus;
+        }
     }
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
